b5ea82acb243d45c90b2d5fa0428bf6832f7b7ab
veronika <veronikamuk@gmail.com>
2025-10-29 22:48:11 +0700

    ggwp2

--- src/array_function.c
+++ src/array_function.c
@@ -1,5 +1,4 @@
 #include "array_function.h"
-
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; i++) {
         dest[i] = src1[i];
@@ -36,10 +35,7 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     }
 }
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-    if (size == 0) {
-        return;
-    }
-    
+   
     *min = src[0];
     *max = src[0];
     
