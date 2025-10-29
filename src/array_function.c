c25e33a6b0a9c5ad19e37876f327fd409b5ded9a
veronika <veronikamuk@gmail.com>
2025-10-29 22:37:57 +0700

    ggwp

--- src/array_function.c
+++ src/array_function.c
@@ -1,3 +1,5 @@
+#include "array_function.h"
+
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; i++) {
         dest[i] = src1[i];
