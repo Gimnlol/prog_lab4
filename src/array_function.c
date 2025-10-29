acfb0bc073157fe4822a9e20709c5ff9119aa902
veronika <veronikamuk@gmail.com>
2025-10-29 22:27:52 +0700

    ggwp

--- src/array_function.c
+++ src/array_function.c
@@ -1,4 +1,3 @@
-// Задание 1
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; i++) {
         dest[i] = src1[i];
@@ -9,7 +8,6 @@ void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, i
     }
 }
 
-// Задание 2
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     size_t i = 0, j = 0, k = 0;
     
@@ -37,7 +35,6 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     }
 }
 
-// Задание 3
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     if (size == 0) {
         return;
